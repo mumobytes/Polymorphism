53dd08626ebc4f5b89c6c5d01432cc617bcb44a3
mumox404 <125362116+mumobytes@users.noreply.github.com>
2025-09-08 15:21:50 +0300

    Create polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,17 @@
+class Car:
+    def move(self):
+        print("Driving ")
+
+class Plane:
+    def move(self):
+        print("Flying ")
+
+class Boat:
+    def move(self):
+        print("Sailing")
+
+# Polymorphism in action
+vehicles = [Car(), Plane(), Boat()]
+
+for vehicle in vehicles:
+    vehicle.move()
